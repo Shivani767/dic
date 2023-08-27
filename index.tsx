c98d3caeb4c9e7d1a0d5997cddd1481e1096ae9a
Shivani767 <101629653+Shivani767@users.noreply.github.com>
2023-08-27 19:12:34 +0530

    login

--- index.tsx
+++ index.tsx
@@ -1,24 +1,49 @@
 import "./style.css";
 import doctor from "../../assets/images/doctor.png";
+ // eslint-disable-next-line
 import PrimaryButton from "../../components/primary-button";
 
 const Index = () => {
   return (
-    <div className="section-hero">
-      <div className="section-hero__img-box">
+    <div className="login">
+      <div className="login__img-box">
         <img src={doctor} alt="doctor" />
       </div>
-      <div className="section-hero__content-box">
-        <h1 className="heading-primary">Heading</h1>
-        <p className="paragraph-primary">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
-        </p>
-        <PrimaryButton title="Explore" destination="/" />
+      <div className="login__content-box">
+        <h1 className="heading-primary">Login</h1>
+        <form action="" className="login__content-box__form">
+          <div>
+            <label htmlFor="username">
+              Username:
+              <br />
+              <input type="text" name="username" id="username" />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="password">
+              Password:
+              <br />
+              <input type="password" name="password" id="password" />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="department">
+              Department:
+              <br />
+              <select name="department" id="department">
+                <option>Department</option>
+                <option value="a">A</option>
+                <option value="b">B</option>
+                <option value="c">C</option>
+              </select>
+            </label>
+          </div>
+          <div>
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
