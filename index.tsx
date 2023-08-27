4622f98a7a8a3bb7f699a5860bddcf56ab0ded8b
Shivani767 <101629653+Shivani767@users.noreply.github.com>
2023-08-27 19:11:18 +0530

    hero section

--- index.tsx
+++ index.tsx
@@ -1,24 +1,26 @@
-import { Fragment } from "react";
-import { Routes, Route } from "react-router-dom";
-import AppHeader from "../layout/app-header";
-import Login from "../pages/login";
-import Hero from "../pages/hero-section";
-import PatientDetails from "../pages/patient-details";
-import Patient from "../pages/patient-view";
-import PatientList from "../pages/patient-list";
+import "./style.css";
+import doctor from "../../assets/images/doctor.png";
+import PrimaryButton from "../../components/primary-button";
 
 const Index = () => {
   return (
-    <Fragment>
-      <AppHeader />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/overview" element={<Hero />} />
-        <Route path="/patient-details" element={<PatientDetails />} />
-        <Route path="/patient" element={<Patient />} />
-        <Route path="/patient-list" element={<PatientList />} />
-      </Routes>
-    </Fragment>
+    <div className="section-hero">
+      <div className="section-hero__img-box">
+        <img src={doctor} alt="doctor" />
+      </div>
+      <div className="section-hero__content-box">
+        <h1 className="heading-primary">Heading</h1>
+        <p className="paragraph-primary">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged.
+        </p>
+        <PrimaryButton title="Explore" destination="/" />
+      </div>
+    </div>
   );
 };
 
