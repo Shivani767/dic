05af46591ce07aa2fd77a1ea6bd8305062113a02
Shivani767 <101629653+Shivani767@users.noreply.github.com>
2023-08-27 19:21:34 +0530

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,51 +1,24 @@
 import "./style.css";
-import doctor from "../../assets/images/doctor.png";
- // eslint-disable-next-line
-import PrimaryButton from "../../components/primary-button";
+import { Link } from "react-router-dom";
 
 const Index = () => {
   return (
-    <div className="login">
-      <div className="login__img-box">
-        <img src={doctor} alt="doctor" />
+    <header className="header">
+      <div className="header__navigation">
+        <ul>
+          <li className="header__navigation__logo-box">
+            <Link to="/">DIC</Link>
+          </li>
+          <li>Home</li>
+          <li>Upload Details</li>
+          <li>Patient Details</li>
+          <li>About Us</li>
+        </ul>
       </div>
-      <div className="login__content-box">
-        <h1 className="heading-primary">Login</h1>
-        <form action="" className="login__content-box__form">
-          <div>
-            <label htmlFor="username">
-              Username:
-              <br />
-              <input type="text" name="username" id="username" />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="password">
-              Password:
-              <br />
-              <input type="password" name="password" id="password" />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="department">
-              Department:
-              <br />
-              <select name="department" id="department">
-                <option>Department</option>
-                <option value="a">A</option>
-                <option value="b">B</option>
-                <option value="c">C</option>
-              </select>
-            </label>
-          </div>
-          <div>
-            <button type="submit" className="btn-primary">
-              Login
-            </button>
-          </div>
-        </form>
+      <div className="header__btn">
+        <Link to="/contact">Contact Us</Link>
       </div>
-    </div>
+    </header>
   );
 };
 
