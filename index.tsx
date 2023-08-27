a8b57d7e4c340f2fb7e244219116cea2ed9fd0a0
Shivani767 <101629653+Shivani767@users.noreply.github.com>
2023-08-27 19:41:14 +0530

    pat list

--- index.tsx
+++ index.tsx
@@ -1,61 +1,14 @@
 import "./style.css";
-import patient from "../../assets/images/patient.png";
+import PatientItem from "../../components/patient";
 
 const Index = () => {
   return (
-    <div className="section-patient-details">
-      <div className="section-patient-details__content">
-        <h1 className="heading-primary">Patient Details</h1>
-        <form action="" className="section-patient-details__form">
-          <div>
-            <label htmlFor="name">
-              name:
-              <br />
-              <input type="text" name="name" id="name" />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="age">
-              Age:
-              <br />
-              <input type="number" name="age" id="age" />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="report">
-              View Report:
-              <br />
-              <input
-                type="button"
-                name="report"
-                id="report"
-                value="View Report"
-              />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="gltf">
-              View GLTF:
-              <br />
-              <input type="button" name="gltf" id="gltf" value="View GLTF" />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="prescription">
-              Add Prescription:
-              <br />
-              <input type="file" name="prescription" id="prescription" />
-            </label>
-          </div>
-          <div>
-            <button type="submit" className="btn-primary">
-              Save
-            </button>
-          </div>
-        </form>
-      </div>
-      <div className="section-patient-details__img-box">
-        <img src={patient} alt="patient details" />
+    <div className="patient-list">
+      <h1 className="heading-primary">Patient Details</h1>
+      <div className="patient-list__content">
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map(() => (
+          <PatientItem />
+        ))}
       </div>
     </div>
   );
