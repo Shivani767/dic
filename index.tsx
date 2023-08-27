c16121283d10718471a1d64964c8a9ceaf723c81
Shivani767 <101629653+Shivani767@users.noreply.github.com>
2023-08-27 19:46:19 +0530

    cmp primary buttn

--- index.tsx
+++ index.tsx
@@ -1,26 +1,16 @@
 import "./style.css";
-import doctor from "../../assets/images/doctor.png";
+import { Link } from "react-router-dom";
 
-const Index = () => {
+type PropsType = {
+  title: string;
+  destination: string;
+};
+
+const Index = ({ title, destination }: PropsType) => {
   return (
-    <div className="patient-item__profile">
-      <div className="patient-item__profile__img-box">N
-        <img src={doctor} alt="patient" />
-      </div>
-      <div className="patient-item__profile__content-box">
-        <h2 className="heading-secondary">John Doe</h2>
-        <p className="paragraph-primary">Age: 21</p>
-        <p style={{ marginTop: "1rem" }} className="paragraph-primary">
-          Description:
-          <br />
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.
-        </p>
-        <button className="patient-item__profile__content-box__btn">
-          Details
-        </button>
-      </div>
-    </div>
+    <Link to={`/${destination}`} className="btn-primary">
+      {title}
+    </Link>
   );
 };
 
