bce10caf0e4cdd7b03a79b0b3c676053b2a6d459
Shivani767 <101629653+Shivani767@users.noreply.github.com>
2023-08-27 19:36:33 +0530

    pat det

--- index.tsx
+++ index.tsx
@@ -1,24 +1,61 @@
 import "./style.css";
-import doctor from "../../assets/images/doctor.png";
+import patient from "../../assets/images/patient.png";
 
 const Index = () => {
   return (
-    <div className="patient-item__profile">
-      <div className="patient-item__profile__img-box">N
-        <img src={doctor} alt="patient" />
+    <div className="section-patient-details">
+      <div className="section-patient-details__content">
+        <h1 className="heading-primary">Patient Details</h1>
+        <form action="" className="section-patient-details__form">
+          <div>
+            <label htmlFor="name">
+              name:
+              <br />
+              <input type="text" name="name" id="name" />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="age">
+              Age:
+              <br />
+              <input type="number" name="age" id="age" />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="report">
+              View Report:
+              <br />
+              <input
+                type="button"
+                name="report"
+                id="report"
+                value="View Report"
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="gltf">
+              View GLTF:
+              <br />
+              <input type="button" name="gltf" id="gltf" value="View GLTF" />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="prescription">
+              Add Prescription:
+              <br />
+              <input type="file" name="prescription" id="prescription" />
+            </label>
+          </div>
+          <div>
+            <button type="submit" className="btn-primary">
+              Save
+            </button>
+          </div>
+        </form>
       </div>
-      <div className="patient-item__profile__content-box">
-        <h2 className="heading-secondary">John Doe</h2>
-        <p className="paragraph-primary">Age: 21</p>
-        <p style={{ marginTop: "1rem" }} className="paragraph-primary">
-          Description:
-          <br />
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.
-        </p>
-        <button className="patient-item__profile__content-box__btn">
-          Details
-        </button>
+      <div className="section-patient-details__img-box">
+        <img src={patient} alt="patient details" />
       </div>
     </div>
   );
